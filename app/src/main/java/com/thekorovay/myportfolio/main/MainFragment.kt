a06a54fef4e3d90567cd0af6416919593e7d0ef1
Evgeniy Bezkorovayniy <thirteenisluckynumber@gmail.com>
2021-02-18 18:09:56 +0300

    Added 'About' screen, resources and actions for it

--- app/src/main/java/com/thekorovay/myportfolio/main/MainFragment.kt
+++ app/src/main/java/com/thekorovay/myportfolio/main/MainFragment.kt
@@ -3,12 +3,12 @@ package com.thekorovay.myportfolio.main
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.thekorovay.myportfolio.R
 import com.thekorovay.myportfolio.databinding.FragmentMainBinding
 
@@ -28,9 +28,21 @@ class MainFragment: Fragment() {
             false
         )
 
+        setHasOptionsMenu(true)
+
         setCards()
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.about_app_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     private fun setCards() {
