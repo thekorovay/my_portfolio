d4b76bb1165f6d94af937b7ba49eb1e7047661a6
Evgeniy Bezkorovayniy <thirteenisluckynumber@gmail.com>
2021-04-17 14:44:26 +0300

    Updated some interface elements

--- app/src/main/java/com/thekorovay/myportfolio/module_news/ui/AboutFragment.kt
+++ app/src/main/java/com/thekorovay/myportfolio/module_news/ui/AboutFragment.kt
@@ -11,6 +11,7 @@ import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialSharedAxis
 import com.thekorovay.myportfolio.R
 import com.thekorovay.myportfolio.databinding.FragmentAboutBinding
 import com.thekorovay.myportfolio.tools.setupNavUpButton
@@ -36,6 +37,13 @@ class AboutFragment : Fragment() {
         setContacts()
 
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
     }
 
     private fun setContacts() {
